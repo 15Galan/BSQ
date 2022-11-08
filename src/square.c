8302a3c2b6963459854c0cdfd2d403d0f9fb1c4f
Galán <antoniogalanG15@gmail.com>
2022-11-09 00:48:34 +0100

    Método 'can_grow()' arreglado
    Ahora se calcula una solución de forma correcta.

--- src/square.c
+++ src/square.c
@@ -6,7 +6,7 @@
 /*   By: antgalan <antgalan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 12:43:19 by antgalan          #+#    #+#             */
-/*   Updated: 2022/11/08 21:19:12 by antgalan         ###   ########.fr       */
+/*   Updated: 2022/11/09 00:47:29 by antgalan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,9 @@ t_square	*init_square(int x, int y)
 {
 	t_square	*sqr;
 
-	sqr = (t_square *) malloc(sizeof(t_square));
+	sqr = malloc(sizeof(t_square));
+	if (sqr == NULL)
+		return (NULL);
 	sqr->x = x;
 	sqr->y = y;
 	sqr->d = 1;
@@ -30,22 +32,19 @@ int	can_grow(int **map, t_square sqr, t_caption cap)
 	int	i;
 	int	j;
 
-	i = sqr.x + sqr.d;
-	j = sqr.y + sqr.d;
-	if (cap.num_rows < i || cap.num_columns < j)
+	i = sqr.x;
+	while (i < sqr.x + sqr.d)
+	{
+		j = sqr.y;
+		while (j < sqr.y + sqr.d)
+		{
+			if (map[i][j] == 1)
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	if (cap.num_rows <= sqr.x + sqr.d || cap.num_columns <= sqr.y + sqr.d)
 		return (0);
-	while (sqr.x <= i)
-	{
-		if (map[i][j] == 1)
-			return (0);
-		i--;
-	}
-	i = sqr.x + sqr.d;
-	while (sqr.y <= j)
-	{
-		if (map[i][j] == 1)
-			return (0);
-		j--;
-	}
 	return (1);
 }
