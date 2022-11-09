d94e4026dcb8017057e78afd316112cc705a8b09
Galán <antoniogalanG15@gmail.com>
2022-11-09 13:27:38 +0100

    Tratamiento de mapas nulos
    Se arrastra el puntero hasta el 'main()'.

--- src/bsq.c
+++ src/bsq.c
@@ -6,7 +6,7 @@
 /*   By: antgalan <antgalan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 08:51:48 by ernesmar          #+#    #+#             */
-/*   Updated: 2022/11/09 10:55:01 by antgalan         ###   ########.fr       */
+/*   Updated: 2022/11/09 13:28:22 by antgalan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,10 +28,10 @@ int	**create_map_from_file(char *path, t_caption *caption, int *flag_error);
 // printf("status error: %d\n", flag_error);
 int	main(int argc, char **argv)
 {
-	int			**map;
 	t_caption	caption;
-	int			flag_error;
 	t_square	*bsq;
+	int			**map;
+	int			flag_error;
 
 	flag_error = 0;
 	if (argc == 2)
@@ -43,7 +43,10 @@ int	main(int argc, char **argv)
 		else if (map != NULL)
 		{
 			bsq = find_max_square(map, caption);
-			print_solution(map, *bsq, caption, ' ');
+			if (bsq != NULL)
+				print_solution(map, *bsq, caption, ' ');
+			else
+				ft_putstr("map error\n");
 		}
 	}
 }
