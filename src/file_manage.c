3aaff02dd4a7e035a0133044c32d7b7c441f0df7
Galán <antoniogalanG15@gmail.com>
2022-11-09 14:31:32 +0100

    archivo normalizado

--- src/file_manage.c
+++ src/file_manage.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   file_manage.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ernesmar <ernesmar@student.42malaga.com>   +#+  +:+       +#+        */
+/*   By: antgalan <antgalan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 16:23:29 by ernesmar          #+#    #+#             */
-/*   Updated: 2022/11/09 13:32:13 by ernesmar         ###   ########.fr       */
+/*   Updated: 2022/11/09 14:31:09 by antgalan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ int	calculate_file_size(char *path)
 	total_size = 0;
 	temp_buffer = (char *)malloc(sizeof(char *) * increase);
 	fd = open(path, O_RDONLY);
-	if(temp_buffer == NULL || fd == -1)
+	if (temp_buffer == NULL || fd == -1)
 	{
 		close(fd);
 		return (-1);
@@ -35,7 +35,7 @@ int	calculate_file_size(char *path)
 	while (attempt != 0)
 	{
 		attempt = read(fd, temp_buffer, increase);
-		if(attempt != 0)
+		if (attempt != 0)
 			total_size += increase;
 	}
 	close(fd);
@@ -50,7 +50,7 @@ char	*read_file(char *path)
 	char	*buffer;
 
 	file_size = calculate_file_size(path);
-	if(file_size <= 0)
+	if (file_size <= 0)
 		return (NULL);
 	fd = open(path, O_RDONLY);
 	buffer = (char *)malloc(sizeof(char *) * file_size);
