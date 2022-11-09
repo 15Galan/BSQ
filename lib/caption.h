a2e05e63563e700dafc682b62aa365f7f7f01427
Galán <antoniogalanG15@gmail.com>
2022-11-09 02:16:43 +0100

    Librerías documentadas y modificadas
    Se añadieron y modificaron comentarios de documentación en los headers.
    Se refactorizaron algunos nombres de variables y funciones.

--- lib/caption.h
+++ lib/caption.h
@@ -6,22 +6,29 @@
 /*   By: antgalan <antgalan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 12:27:49 by ernesmar          #+#    #+#             */
-/*   Updated: 2022/11/08 21:25:13 by antgalan         ###   ########.fr       */
+/*   Updated: 2022/11/09 01:48:16 by antgalan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CAPTION_H
 # define CAPTION_H
 
-/*
- * @brew to save parameters of the origin file
+/**
+ * @brief	Contains information about a map.
+ * @cite	It's found in the first line of the map file.
+ * 
+ * @param rows      Number of rows in the map.
+ * @param cols      Number of columns in the map.
+ * @param empty     Character used to represent empty spaces.
+ * @param obstacle  Character used to represent obstacles.
+ * @param full      Character used to represent solution spaces.
  */
-typedef struct t_caption
+typedef struct s_caption
 {
-	int		num_rows;
-	int		num_columns;
+	int		rows;
+	int		cols;
 	char	empty;
-	char	obstacle;
+	char	obst;
 	char	full;
 }	t_caption;
 
--- lib/file_manage.h
+++ lib/file_manage.h
@@ -3,27 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   file_manage.h                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ernesmar <ernesmar@student.42malaga.com>   +#+  +:+       +#+        */
+/*   By: antgalan <antgalan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 16:25:30 by ernesmar          #+#    #+#             */
-/*   Updated: 2022/11/08 16:33:29 by ernesmar         ###   ########.fr       */
+/*   Updated: 2022/11/09 02:04:34 by antgalan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FILE_MANAGE_H
 # define FILE_MANAGE_H
 
-/*
- * @param path of file to calculate size
- *
- * @return size of file in path or -1 if not readable
+/**
+ *  @brief  Gets the size of a file.
+ * 
+ * @param path  Path of the file to get the size.
+ * 
+ * @return  The size of the file; -1 if not readable.
  */
 int		calculate_file_size(char *path);
 
-/*
- * @param path of file to read
- *
- * @return string inside the file in path or NULL if not readable
+/**
+ * @brief  Reads a file.
+ * 
+ * @param path  Path of the file to read.
+ * 
+ * @return  The string inside the file; NULL if not readable.
  */
 char	*read_file(char *path);
 
