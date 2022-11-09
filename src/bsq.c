910d09f64171fb3c203dcf9a2633f6c1d1f4c4d2
Galán <antoniogalanG15@gmail.com>
2022-11-09 02:18:17 +0100

    Código fuente comentado y modificado
    Se modificó la sintaxis de algunos comentarios.
    Se reorganizó el código fuente.

--- src/bsq.c
+++ src/bsq.c
@@ -6,7 +6,7 @@
 /*   By: antgalan <antgalan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 08:51:48 by ernesmar          #+#    #+#             */
-/*   Updated: 2022/11/08 21:11:54 by antgalan         ###   ########.fr       */
+/*   Updated: 2022/11/09 02:08:20 by antgalan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,14 @@
 
 int	**create_map_from_file(char *path, t_caption *caption, int *flag_error);
 
-//t_square not declared until merge:
-//t_square max_square;
-//flag_error = 0 not necesary. Just for security but redundat
-//
-//print info about map
-//printf("%c %c %c col: %d rows: %d\n", caption.empty, 
-//caption.obstacle, caption.full, caption.num_columns, caption.num_rows);
-//printf("status error: %d\n", flag_error);
+// t_square not declared until merge:
+// t_square max_square;
+// flag_error = 0 not necesary. Just for security but redundat
+// 
+// print info about map
+// printf("%c %c %c col: %d rows: %d\n", caption.empty, 
+// caption.obstacle, caption.full, caption.num_columns, caption.num_rows);
+// printf("status error: %d\n", flag_error);
 int	main(int argc, char **argv)
 {
 	int			**map;
--- src/create_map.c
+++ src/create_map.c
@@ -6,7 +6,7 @@
 /*   By: antgalan <antgalan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 15:03:11 by ernesmar          #+#    #+#             */
-/*   Updated: 2022/11/08 21:33:11 by antgalan         ###   ########.fr       */
+/*   Updated: 2022/11/09 02:09:00 by antgalan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ int	calculate_rows(char *raw)
 	return (count);
 }
 
-//raw is the raw text in the file or the stdin in the form
+// raw is the raw text in the file or the stdin in the form
 // NCCC ...(next part of the map);
 // where N is number of lines
 // C is a character meaning something in the map
@@ -43,22 +43,22 @@ t_caption	fill_caption(char *raw, int *flag_error)
 	int			displacement;
 
 	displacement = get_base(atoi(raw));
-	new_caption.num_rows = atoi(raw);
-	new_caption.num_columns = calculate_rows(raw);
+	new_caption.rows = atoi(raw);
+	new_caption.cols = calculate_rows(raw);
 	new_caption.empty = raw[displacement + 0];
-	new_caption.obstacle = raw[displacement + 1];
+	new_caption.obst = raw[displacement + 1];
 	new_caption.full = raw[displacement + 2];
 	if (raw[displacement + 4] != '\n')
 		*flag_error = 1;
 	else if (ft_is_printable(new_caption.empty) == 0)
 		*flag_error = 1;
-	else if (ft_is_printable(new_caption.obstacle) == 0)
+	else if (ft_is_printable(new_caption.obst) == 0)
 		*flag_error = 1;
 	else if (ft_is_printable(new_caption.full) == 0)
 		*flag_error = 1;
-	else if (new_caption.empty != new_caption.obstacle)
+	else if (new_caption.empty != new_caption.obst)
 		*flag_error = 1;
-	else if (new_caption.obstacle != new_caption.full)
+	else if (new_caption.obst != new_caption.full)
 		*flag_error = 1;
 	else if (new_caption.full != new_caption.empty)
 		*flag_error = 1;
@@ -69,17 +69,17 @@ int	elem_from_caption(char element, t_caption caption)
 {
 	if (element == caption.empty)
 		return (0);
-	else if (element == caption.obstacle)
+	else if (element == caption.obst)
 		return (1);
 	else
 		return (-1);
 }
 
-//fill the maps and check if is correct.
-//if is not correct, error = -1
-//
-//*
-//if(raw[displacement + 4] != '\n')
+// fill the maps and check if is correct.
+// if is not correct, error = -1
+// 
+// *
+// if(raw[displacement + 4] != '\n')
 //        *flag_error = 1
 void	fill_map(int **map, t_caption *caption, char *raw, int *flag_error)
 {
@@ -92,12 +92,12 @@ void	fill_map(int **map, t_caption *caption, char *raw, int *flag_error)
 	j = -1;
 	while (raw[raw_pos + 1] != '\n')
 		raw_pos++;
-	while (raw[(++raw_pos) + 1] != '\0' && j < caption->num_rows)
+	while (raw[(++raw_pos) + 1] != '\0' && j < caption->rows)
 	{
 		if (raw[raw_pos] == '\n')
 		{
-			map[++j] = (int *)malloc(sizeof(int) * caption->num_columns);
-			*flag_error = (i != caption->num_columns && j - 1 != 0);
+			map[++j] = (int *)malloc(sizeof(int) * caption->cols);
+			*flag_error = (i != caption->cols && j - 1 != 0);
 			i = 0;
 		}
 		else
@@ -109,8 +109,8 @@ void	fill_map(int **map, t_caption *caption, char *raw, int *flag_error)
 	*flag_error += (raw[(++raw_pos) + 1] != '\0');
 }
 
-//if we cannot access the file, we cannot create a map,
-//so we do not have to free it 
+// if we cannot access the file, we cannot create a map,
+// so we do not have to free it 
 int	**create_map_from_file(char *path,
 	t_caption *caption, int *flag_error)
 {
@@ -124,7 +124,7 @@ int	**create_map_from_file(char *path,
 		return (NULL);
 	}
 	*caption = fill_caption(raw, flag_error);
-	map = (int **)malloc(sizeof(int *) * (caption->num_rows));
+	map = (int **)malloc(sizeof(int *) * (caption->rows));
 	fill_map(map, caption, raw, flag_error);
 	free(raw);
 	return (map);
